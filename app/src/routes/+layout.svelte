536b3b60210fdc4e6ff75d282640df6ffc434938
José Anés <anes@cheetah.local>
2025-04-29 20:21:36 -0400

    chore: add CI/CD workflow and infra stack

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<!-- app/src/routes/+layout.svelte -->
+<script lang="ts">
+  /*  Import the global Tailwind stylesheet once  */
+  import "../app.css";
+</script>
+
+<!-- Everything in the app will be rendered here -->
+<slot />
+
+
+
+
+
---
+++ infra/lib/garage-stack.ts
@@ -0,0 +1,31 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { Bucket, BucketEncryption, RemovalPolicy } from 'aws-cdk-lib/aws-s3';
+import { Distribution, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+
+export class GarageStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Private bucket to hold the static site
+    const siteBucket = new Bucket(this, 'SiteBucket', {
+      websiteIndexDocument: 'index.html',
+      encryption: BucketEncryption.S3_MANAGED,
+      publicReadAccess: false,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+
+    // CloudFront distro in front of the bucket
+    new Distribution(this, 'CDN', {
+      defaultBehavior: {
+        origin: new S3Origin(siteBucket),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS
+      }
+    });
+
+    new cdk.CfnOutput(this, 'BucketName', { value: siteBucket.bucketName });
+  }
+}
+
