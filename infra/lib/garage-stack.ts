97027262753def67f798fbf9f88271966c445b63
José Anés <anes@cheetah.local>
2025-04-29 22:20:43 -0400

    fix: make S3 bucket world-readable for static site

--- infra/lib/garage-stack.ts
+++ infra/lib/garage-stack.ts
@@ -1,8 +1,8 @@
+// infra/lib/garage-stack.ts
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-
 import { RemovalPolicy } from 'aws-cdk-lib';
-import { Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
+import { Bucket, BucketEncryption, BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
 import { Distribution, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
 import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 
@@ -10,16 +10,20 @@ export class GarageStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // --- S3 bucket for the static site ---
+    /* ---------- S3 bucket ---------- */
     const siteBucket = new Bucket(this, 'SiteBucket', {
       websiteIndexDocument: 'index.html',
       encryption: BucketEncryption.S3_MANAGED,
-      publicReadAccess: false,
+
+      /**  ---- PUBLIC READ, STATIC-SITE STYLE ----  */
+      publicReadAccess: true,              // ⬅️ add this line
+      blockPublicAccess: BlockPublicAccess.NONE, // ⬅️ make sure this is NONE
+
       removalPolicy: RemovalPolicy.DESTROY,
       autoDeleteObjects: true
     });
 
-    // --- CloudFront distribution in front of the bucket ---
+    /* ---------- CloudFront ---------- */
     const cdn = new Distribution(this, 'CDN', {
       defaultBehavior: {
         origin: new S3Origin(siteBucket),
@@ -27,14 +31,8 @@ export class GarageStack extends cdk.Stack {
       }
     });
 
-    // --- Outputs so we can find them later ---
-    new cdk.CfnOutput(this, 'BucketName', {
-      value: siteBucket.bucketName
-    });
-
-    new cdk.CfnOutput(this, 'CDNDistributionDomainName', {
-      value: cdn.domainName
-    });
+    new cdk.CfnOutput(this, 'BucketName', { value: siteBucket.bucketName });
+    new cdk.CfnOutput(this, 'CDNDistributionDomainName', { value: cdn.domainName });
   }
 }
 
