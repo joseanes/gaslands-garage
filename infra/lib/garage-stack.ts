c218896064683cde431b9d0cd81eb2e2b1291653
José Anés <anes@cheetah.local>
2025-04-29 22:02:33 -0400

    feat: re-add S3 bucket and CloudFront to InfraStack

--- infra/lib/garage-stack.ts
+++ infra/lib/garage-stack.ts
@@ -1,15 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
 import { RemovalPolicy } from 'aws-cdk-lib';
 import { Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
 import { Distribution, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
 import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
 
 export class GarageStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Private bucket to hold the static site
+    // --- S3 bucket for the static site ---
     const siteBucket = new Bucket(this, 'SiteBucket', {
       websiteIndexDocument: 'index.html',
       encryption: BucketEncryption.S3_MANAGED,
@@ -18,15 +19,22 @@ export class GarageStack extends cdk.Stack {
       autoDeleteObjects: true
     });
 
-    // CloudFront distro in front of the bucket
-    new Distribution(this, 'CDN', {
+    // --- CloudFront distribution in front of the bucket ---
+    const cdn = new Distribution(this, 'CDN', {
       defaultBehavior: {
         origin: new S3Origin(siteBucket),
         viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS
       }
     });
 
-    new cdk.CfnOutput(this, 'BucketName', { value: siteBucket.bucketName });
+    // --- Outputs so we can find them later ---
+    new cdk.CfnOutput(this, 'BucketName', {
+      value: siteBucket.bucketName
+    });
+
+    new cdk.CfnOutput(this, 'CDNDistributionDomainName', {
+      value: cdn.domainName
+    });
   }
 }
 
