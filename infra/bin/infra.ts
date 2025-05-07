afe9935ed42998639f6289c4faf1afdf09bfdefc
José Anés <anes@cheetah.local>
2025-05-07 16:38:12 -0400

    feat: add custom domain support for gaslandsgarage.com

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -2,5 +2,11 @@ import * as cdk from 'aws-cdk-lib';
 import { GarageStack } from '../lib/garage-stack';
 
 const app = new cdk.App();
-new GarageStack(app, 'InfraStack', {});   // keep the stack name but use GarageStack class
+new GarageStack(app, 'InfraStack', {
+  domainName: 'gaslandsgarage.com',
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+});   // keep the stack name but use GarageStack class
 
--- infra/lib/garage-stack.ts
+++ infra/lib/garage-stack.ts
@@ -9,15 +9,31 @@ import {
 } from 'aws-cdk-lib/aws-s3';
 import {
   Distribution,
-  ViewerProtocolPolicy
+  ViewerProtocolPolicy,
+  AllowedMethods,
+  CachePolicy,
+  OriginRequestPolicy,
+  SecurityPolicyProtocol,
+  SSLMethod,
+  Certificate
 } from 'aws-cdk-lib/aws-cloudfront';
 import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { RemovalPolicy } from 'aws-cdk-lib';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as route53Targets from 'aws-cdk-lib/aws-route53-targets';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+
+export interface GarageStackProps extends cdk.StackProps {
+  domainName?: string;
+}
 
 export class GarageStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: GarageStackProps) {
     super(scope, id, props);
+    
+    // Define domain name
+    const domainName = props?.domainName || 'gaslandsgarage.com';
 
     /* ----------------------------------------------------------------
        S3 bucket for the static site
@@ -49,28 +65,70 @@ export class GarageStack extends cdk.Stack {
     );
 
     /* ----------------------------------------------------------------
+       Route53 hosted zone (use existing zone)
+       ---------------------------------------------------------------- */
+    // Look up the hosted zone for the domain
+    const hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
+      domainName: domainName,
+    });
+
+    /* ----------------------------------------------------------------
+       ACM Certificate for HTTPS
+       ---------------------------------------------------------------- */
+    // Create certificate in us-east-1 (required for CloudFront)
+    const certificate = new acm.DnsValidatedCertificate(this, 'SiteCertificate', {
+      domainName: domainName,
+      hostedZone,
+      region: 'us-east-1', // CloudFront requires certificates in us-east-1
+      subjectAlternativeNames: [`www.${domainName}`] // Include www subdomain
+    });
+
+    /* ----------------------------------------------------------------
        CloudFront distribution
        ---------------------------------------------------------------- */
-const cdn = new Distribution(this, 'CDN', {
-  defaultBehavior: {
-    origin: new S3Origin(siteBucket),
-    viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS
-  },
-  errorResponses: [
-    {
-      httpStatus: 403,
-      responseHttpStatus: 200,
-      responsePagePath: '/index.html',
-      ttl: cdk.Duration.minutes(1)
-    },
-    {
-      httpStatus: 404,
-      responseHttpStatus: 200,
-      responsePagePath: '/index.html',
-      ttl: cdk.Duration.minutes(1)
-    }
-  ]
-});
+    const cdn = new Distribution(this, 'CDN', {
+      defaultBehavior: {
+        origin: new S3Origin(siteBucket),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+      },
+      domainNames: [domainName, `www.${domainName}`],
+      certificate: certificate,
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+          ttl: cdk.Duration.minutes(1)
+        },
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+          ttl: cdk.Duration.minutes(1)
+        }
+      ],
+      minimumProtocolVersion: SecurityPolicyProtocol.TLS_V1_2_2021
+    });
+    
+    /* ----------------------------------------------------------------
+       Route53 DNS records
+       ---------------------------------------------------------------- */
+    // Create A record for apex domain (example.com)
+    new route53.ARecord(this, 'SiteAliasRecord', {
+      recordName: domainName,
+      target: route53.RecordTarget.fromAlias(
+        new route53Targets.CloudFrontTarget(cdn)
+      ),
+      zone: hostedZone
+    });
+    
+    // Create CNAME record for www subdomain
+    new route53.CnameRecord(this, 'WWWRecord', {
+      recordName: `www.${domainName}`,
+      domainName: domainName,
+      zone: hostedZone
+    });
 
     /* ----------------------------------------------------------------
        Outputs
@@ -81,6 +139,14 @@ const cdn = new Distribution(this, 'CDN', {
 
     new cdk.CfnOutput(this, 'CDNDistributionDomainName', {
       value: cdn.domainName
+    });
+    
+    new cdk.CfnOutput(this, 'CustomDomain', {
+      value: `https://${domainName}`
+    });
+    
+    new cdk.CfnOutput(this, 'CustomDomainWWW', {
+      value: `https://www.${domainName}`
     });
   }
 }
